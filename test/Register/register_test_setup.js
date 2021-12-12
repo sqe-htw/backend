a7c2bb0205d0a6e6f4aa46ddc80e97163687a8a5
Tristan Glaes <trglae@gmail.com>
2021-12-12 13:31:21 +0100

    Test setup register

---
+++ test/Register/register_test_setup.js
@@ -0,0 +1,3 @@
+const test_setup = require('../test_setup');
+
+test_setup.setUpDatabaseForTest('test/Register/register_test_setup.sql');
---
+++ test/Register/register_test_setup.sql
@@ -0,0 +1,2 @@
+DELETE FROM user;
+INSERT INTO user (username, password) VALUES('Existed-Username', 'ThePasswordDoesNotMatter');
